174e5c01881f0479460e6cfd48bb3e9671ba202f
Julian Trajanson <julian.trajanson@gmail.com>
2016-11-13 01:44:55 -0500

    add compressor

--- LZW.hs
+++ LZW.hs
@@ -11,12 +11,64 @@ startingASCIIDictionary = Map.fromList [tupelize x | x <- [1..127] ]
 
 
 
+
 lzwCompress :: String -> [Integer]
-lzwCompress text = go (startingASCIIDictionary) text [] []
-  where go dictionary [] [] accumulatedCompression = accumulatedCompression
-        go dictionary (x:[]) [] accumulatedCompression = go dictionary [] [x] accumulatedCompression
-        go dictionary (x:remainingText) [] accumulatedCompression = go dictionary (remainingText) [x] accumulatedCompression
-        go dictionary remainingText textInBuffer accumulatedCompression
-         | otherwise = go dictionary (remainingText) [] (accumulatedCompression ++ [compressBuffer dictionary textInBuffer])
-        compressBuffer dictionary textInBuffer
-         | otherwise = fromJust (Map.lookup textInBuffer dictionary)
+lzwCompress text = forwardCompression (startingASCIIDictionary) text [] []
+
+
+-- CONDITIONS
+-- REMAINING TEXT IS EMPTY
+    -- IF NO TEXT IN BUFFER  -----------> RETURN ACCUMULATED COMPRESSION
+    -- IF TEXT IN BUFFER
+        -- IF IN DICTIONARY     --------> PUSH COMPRESSED BUFFER INTO ACCUM
+        --                      --------> RETURN
+        --
+        -- IF NOT IN DICTIONARY --------> PUSH COMPRESSED INIT INTO ACCUM
+        --                      --------> CALL AGAIN
+-- REMAINING TEXT IS FULL
+    -- IF NO TEXT IN BUFFER  ------------> MOVE HEAD OF REMAIN TEXT TO BUFFER
+    --                       ------------> CALL AGAIN
+    -- IF TEXT IN BUFFER
+        -- IF IN DICTIONARY  ------------> APPEND HEAD OF TEXT ONTO BUFFER
+        --                   ------------> CALL AGAIN
+        --
+        --------------------------------------------------------------------------------
+        -- IF NOT IN DICTIONARY ---------> PUSH COMPRESSED INIT INTO ACCUM
+        --                      ---------> CALL AGAIN
+
+
+
+
+
+
+--                 dictionary remainingText textInBuffer accumulatedCompression
+forwardCompression dictionary []            []           accumulatedCompression = accumulatedCompression
+forwardCompression dictionary []            textInBuffer accumulatedCompression
+    | Map.member textInBuffer dictionary = ( accumulatedCompression ++
+                                             [compressBuffer dictionary textInBuffer]
+                                           )
+    | otherwise                          = forwardCompression dictionary
+                                                              []
+                                                              [last textInBuffer]
+                                                              (accumulatedCompression ++
+                                                                [compressBuffer dictionary (init textInBuffer)]
+                                                              )
+forwardCompression dictionary remainingText []           accumulatedCompression = forwardCompression dictionary
+                                                                                                     (tail remainingText)
+                                                                                                     [head remainingText]
+                                                                                                     accumulatedCompression
+forwardCompression dictionary remainingText textInBuffer accumulatedCompression
+    | Map.member textInBuffer dictionary = forwardCompression dictionary
+                                                              (tail remainingText)
+                                                              (textInBuffer ++ [head remainingText])
+                                                              accumulatedCompression
+    | otherwise =                          forwardCompression (Map.insert (textInBuffer)
+                                                                      ((toInteger $ Map.size dictionary) + 1)
+                                                                      dictionary
+                                                              )
+                                                              (remainingText)
+                                                              ([last textInBuffer])
+                                                              (accumulatedCompression ++ [compressBuffer dictionary (init textInBuffer)])
+
+compressBuffer dictionary textInBuffer
+ | otherwise = fromJust (Map.lookup textInBuffer dictionary)
